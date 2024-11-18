d39a95c0e829d9fc58cea8cf71508e41dc0fa391
openneuralforge <samuelwatson@outlook.com.au>
2024-11-19 07:59:06 +1100

    moved to simple test example
    forge@openneuralforge:/mnt/c/git/Anvil/Hammer$ go run .
    Input Neuron 1 set to 1.500000
    Input Neuron 2 set to -2.000000
    === Timestep 0 ===
    Dense Neuron 3: Value=0.450000
    Dense Neuron 4: Value=-0.848284
    RNN Neuron 5: Value=-0.378479
    LSTM Neuron 6: Value=0.077537, CellState=0.148491
    CNN Neuron 7: Output[0]=0.390000
    CNN Neuron 7: Output[1]=0.655343
    CNN Neuron 7: Value=0.522672
    Attention Neuron 8: Weights=[0.33216802945734414 0.28957267762295336 0.3782592929197025]
    Attention Neuron 8: Value=0.094439
    Dense Neuron 9: Value=0.094439
    === Timestep 1 ===
    Dense Neuron 3: Value=0.450000
    Dense Neuron 4: Value=-0.848284
    RNN Neuron 5: Value=-0.650845
    LSTM Neuron 6: Value=0.109353, CellState=0.210969
    CNN Neuron 7: Output[0]=0.390000
    CNN Neuron 7: Output[1]=0.655343
    CNN Neuron 7: Value=0.522672
    Attention Neuron 8: Weights=[0.39636701015077286 0.2626177450599726 0.3410152447892545]
    Attention Neuron 8: Value=-0.051017
    Dense Neuron 9: Value=-0.051017
    === Timestep 2 ===
    Dense Neuron 3: Value=0.450000
    Dense Neuron 4: Value=-0.848284
    RNN Neuron 5: Value=-0.781467
    LSTM Neuron 6: Value=0.122508, CellState=0.237258
    CNN Neuron 7: Output[0]=0.390000
    CNN Neuron 7: Output[1]=0.655343
    CNN Neuron 7: Value=0.522672
    Attention Neuron 8: Weights=[0.4415529558214082 0.24337779704828752 0.3150692471303042]
    Attention Neuron 8: Value=-0.150566
    Dense Neuron 9: Value=-0.150566
    Softmax Applied to Neuron 9: Value=1.000000
    Final Outputs:
    Neuron 9: 1.000000

--- main.go
+++ main.go
@@ -1,51 +1,6 @@
 package main
 
-import (
-	"fmt"
-	"math/rand"
-	"time"
-
-	"blueprint" // Import the blueprint package
-)
-
-// Example JSON configuration for the neural network
-const neuronConfig = `
-[
-    {"id": 1, "type": "input", "value": 0, "connections": []},
-    {"id": 2, "type": "input", "value": 0, "connections": []},
-    {"id": 3, "type": "dense", "bias": 0.1, "activation": "relu", "connections": [[1, 0.5], [2, 0.2]]},
-    {"id": 4, "type": "dense", "bias": -0.1, "activation": "tanh", "connections": [[1, 0.3], [2, 0.8]]},
-    {"id": 5, "type": "rnn", "bias": 0.0, "activation": "tanh", "connections": [[3, 1.0], [4, 1.0]]},
-    {"id": 6, "type": "lstm", "bias": 0.0, "activation": "tanh", "connections": [[3, 1.0], [4, 1.0]]},
-    {"id": 7, "type": "cnn", "bias": 1.0, "activation": "leaky_relu", "connections": [[1, 1.0], [2, 1.0], [3, 1.0], [4, 1.0]]},
-    {"id": 8, "type": "attention", "bias": 0.0, "activation": "linear", "connections": [[5, 1.0], [6, 1.0], [7, 1.0]]},
-    {"id": 9, "type": "output", "bias": 0.0, "activation": "linear", "connections": [[8, 1.0]]}
-]
-`
-
 func main() {
-	rand.Seed(time.Now().UnixNano())
+	simple1()
 
-	// Initialize neural network blueprint
-	bp := blueprint.NewBlueprint()
-
-	// Load neurons from JSON configuration
-	err := bp.LoadNeurons(neuronConfig)
-	if err != nil {
-		fmt.Printf("Error loading neurons: %v\n", err)
-		return
-	}
-
-	// Define input and output nodes
-	bp.AddInputNodes([]int{1, 2})
-	bp.AddOutputNodes([]int{9})
-
-	// Define inputs
-	inputs := map[int]float64{
-		1: 1.5,
-		2: -2.0,
-	}
-
-	// Run the network with specified timesteps for recurrent networks
-	bp.RunNetwork(inputs, 3)
 }
